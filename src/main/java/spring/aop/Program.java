3d82444cc0720e1572156242e70e70ebadf06efb
psj <qkrtkdwns3410>
2021-06-04 23:16:53 +0900

    6-3

--- src/main/java/spring/aop/Program.java
+++ src/main/java/spring/aop/Program.java
@@ -1,5 +1,7 @@
 package spring.aop;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import spring.aop.entity.Exam;
 import spring.aop.entity.NewlecExam;
 
@@ -9,24 +11,30 @@ import java.lang.reflect.Proxy;
 
 public class Program {
     public static void main(String[] args) {
-        Exam exam = new NewlecExam(1, 1, 1, 1);
-        Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), new Class[]{Exam.class}, new InvocationHandler() {
-            @Override//AOP관점지향 프로그밍의 방법입니다.
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                long start = System.currentTimeMillis();
     
-                Object result = method.invoke(exam, args);
-                
-                long end = System.currentTimeMillis();
-                
-                String message = (end - start) + "ms 시간이 걸렸습니다.";
-                
-                System.out.println(message);
-                
-                return result;
-            }
-        });
+        ApplicationContext context = new ClassPathXmlApplicationContext("settingA.xml");
+        Exam proxy = (Exam) context.getBean("proxy");
         System.out.printf("total is %d\n", proxy.total());
-        System.out.printf("avg is %3.2f\n",proxy.avg());
+        System.out.printf("avg is %3.2f\n", proxy.avg());
+        
+//        Exam exam = new NewlecExam(1, 1, 1, 1);
+//
+//        Exam proxy = (Exam) Proxy.newProxyInstance(NewlecExam.class.getClassLoader(), new Class[]{Exam.class}, new InvocationHandler() {
+//
+//            @Override//AOP관점지향 프로그밍의 방법입니다.
+//            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//                long start = System.currentTimeMillis();
+//
+//                Object result = method.invoke(exam, args);
+//
+//                long end = System.currentTimeMillis();
+//
+//                String message = (end - start) + "ms 시간이 걸렸습니다.";
+//
+//                System.out.println(message);
+//
+//                return result;
+//            }
+//        });
     }
 }
